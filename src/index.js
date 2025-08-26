0a7d85ba090f5711e5a42341e62dfe20c12a4d2a
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-08-26 20:49:09 +0200

    created new react/vite project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import navbar from "./components/navbar";
-
-const element = <h1>Hello World</h1>
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(element);
-ReactDOM.createRoot(document.getElementById('root')).render(Navbar);
